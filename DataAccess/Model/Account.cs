a5ee5c1816f63efa66467e037b40c38df0d0d17b
Z1Epime <z1epime@gmx.de>
2023-12-31 13:40:38 +0100

    Added entity "Account"
    Serialization and deserialization in JSON

---
+++ DataAccess/Model/Account.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Model;
+public class Account
+{
+    public double Balance { get; set; }
+
+    public IEnumerable<Entry> Entries { get; set;} = new List<Entry>();
+
+    public IEnumerable<Entry> GetEntriesInDateRange(DateTime begin, DateTime end)
+    {
+        return Entries.Where(e => e.TimeStamp.Ticks >= end.Ticks && e.TimeStamp.Ticks <= begin.Ticks);
+    }
+}
---
+++ DataAccess/Model/Entry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Model;
+public class Entry
+{
+    public DateTime TimeStamp { get; set; }
+
+    public double Value { get; set; }
+}
